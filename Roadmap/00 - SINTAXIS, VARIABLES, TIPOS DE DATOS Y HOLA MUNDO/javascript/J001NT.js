8cd86aedc7528cc39a81e7fa6ab78ccc336c8127
J001NT <basster18@gmail.com>
2024-08-17 19:46:36 -0300

    Se elimina un archivo

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/J001NT.js
@@ -0,0 +1,21 @@
+// https://www.javascript.com/
+
+// asi se comenta una sola linea
+
+/* de esta forma se pueden comentar
+varias lineas en javascript
+nada de lo que este entre la apertura 
+y el cierre, se ejecutara*/
+
+var ejemplo = 1 //esto es una variable
+const ejemplo2 = 2 //esto es una constante
+
+let string = "cadena de texto";
+let number = -1.40;
+let boolean = true;
+let novalor = null;
+let sindefinir;
+let id = Symbol('id');
+let numerogrande = BigInt(12345678012345678123456780);
+
+console.log("!Hola, JavaScript¡");
