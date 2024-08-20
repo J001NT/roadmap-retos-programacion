a2f95300303aff1593910d548bb69290b5cb2ead
J001NT <basster18@gmail.com>
2024-08-19 23:29:29 -0300

    Se continua trabajando en el ejercicio 01

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/J001NT.js
@@ -0,0 +1,29 @@
+//Operadores de asignacion
+let a = 7; // = asigna un valor a una variable
+let b = 8;
+let c = 3;
+
+//Operadores aritmeticos
+console.log(a + b);//adicion
+console.log(b - a);//resta
+console.log(a * b);//multiplicacion
+console.log(b / b);//division
+console.log(b % c);//sobrante o modulo
+
+//Operadores de incremento y decremento
+a++;
+console.log(a);
+b--;
+console.log(b);
+
+//Operadores de asignacion 2
+a += b;
+console.log(a);
+a -= b;
+console.log(a);
+a *= b;
+console.log(a);
+a /= b;
+console.log(a);
+
+//Operadores de comparacion
